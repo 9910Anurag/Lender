366cb723cbef1c57947ec8f8aba7522b6cb4217e
Anurag <9910anurag@gmail.com>
2023-03-30 23:46:28 +0530

    creating api

---
+++ Project.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+const path=require('path')
+const port=3000
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'index.html'))
+
+})
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'login.html'))
+})
+app.listen(port, () => {
+    console.log('example at http://localhost:${port}')
+})
