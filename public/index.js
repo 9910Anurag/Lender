ec957e351ddf792c57d14df1c5a0418ee4d3b8da
9910anurag <9910anurag@gmail.com>
2024-02-12 15:47:45 +0530

    remove dupulicate css files

--- public/index.js
+++ public/index.js
@@ -1,8 +1,16 @@
-let burger = document.querySelector('.burger');
+let burger = document.querySelector('#burger');
 let navli = document.querySelector('.nav-li');
+let close = document.querySelector('#togglebtn');
 burger.addEventListener('click', ()=>{
-   navli.classList.toggle('navliresp');
-   navli.classList.toggle('');
+navli.classList.toggle('navliresp');
+if(close.src.match('menu.png')){
+console.log('click1');
+close.src='x-lg.svg';
+}
+else{
+console.log('clik2');
+close.src='menu.png';
+}
 });
 let image = document.getElementById('image');
         let images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg']
